5b88dda3000a6b7065e1bf30921de96227142dc1
Andrey Vetlugin <antrew@gmail.com>
2015-02-22 16:22:46 +0100

    implemented rolling of graphs and simplified the code in plot_sensors.py

--- plot_sensors.py
+++ plot_sensors.py
@@ -23,66 +23,27 @@ class SensorsBoxLayout(BoxLayout):
     #initialize t
     t = 0
     
-    # ---------------- for ACCELEROMETER sensor --------------------
-    #plot-line for the accelerometer's x values
-    accel_line_x = ObjectProperty(None)
-    #plot-line for the accelerometer's y values
-    accel_line_y = ObjectProperty(None)
-    #plot-line for the accelerometer's z values
-    accel_line_z = ObjectProperty(None)
-    
-    #list for storing the accelerometer's x values
-    accel_px = ListProperty([])
-    #list for storing the accelerometer's y values
-    accel_py = ListProperty([])
-    #list for storing the accelerometer's z values
-    accel_pz = ListProperty([])
-    
-    
-    # ---------------- for GYROSCOPE sensor ----------------------
-    #plot-line for the gyroscope's x values
-    gyro_line_x = ObjectProperty(None)
-    #plot-line for the gyroscope's y values
-    gyro_line_y = ObjectProperty(None)
-    #plot-line for the gyroscope's z values
-    gyro_line_z = ObjectProperty(None)
-    
-    #list for storing the gyroscope's x values
-    gyro_px = ListProperty([])
-    #list for storing the gyroscope's y values
-    gyro_py = ListProperty([])
-    #list for storing the gyroscope's z values
-    gyro_pz = ListProperty([])
-    
-    
     def __init__(self):
         super(BoxLayout, self).__init__()
         self.pt = [self.t]
         
+        self.create_plots()
+        self.roll_plots()
+        self.create_plots()
+
+    def create_plots(self):
         # ---------------- for ACCELEROMETER sensor --------------------
-        self.accel_px = []
-        self.accel_py = []
-        self.accel_pz = []
         self.accel_line_x = MeshLinePlot(color=[1, 0, 0, 1]) # X values are RED
         self.accel_line_y = MeshLinePlot(color=[0, 1, 0, 1]) # Y values are GREEN
         self.accel_line_z = MeshLinePlot(color=[0, 0, 1, 1]) # Z values are BLUE
-        self.accel_line_x.points = zip(self.pt, self.accel_px)
-        self.accel_line_y.points = zip(self.pt, self.accel_py)
-        self.accel_line_z.points = zip(self.pt, self.accel_pz)
         self.ids.accel_graph.add_plot(self.accel_line_x)
         self.ids.accel_graph.add_plot(self.accel_line_y)
         self.ids.accel_graph.add_plot(self.accel_line_z)
         
         # ---------------- for GYROSCOPE sensor ----------------------
-        self.gyro_px = []
-        self.gyro_py = []
-        self.gyro_pz = []
         self.gyro_line_x = MeshLinePlot(color=[1, 0, 0, 1]) # X values are RED
         self.gyro_line_y = MeshLinePlot(color=[0, 1, 0, 1]) # Y values are GREEN
         self.gyro_line_z = MeshLinePlot(color=[0, 0, 1, 1]) # Z values are BLUE
-        self.gyro_line_x.points = zip(self.pt, self.gyro_px)
-        self.gyro_line_y.points = zip(self.pt, self.gyro_py)
-        self.gyro_line_z.points = zip(self.pt, self.gyro_pz)
         self.ids.gyro_graph.add_plot(self.gyro_line_x)
         self.ids.gyro_graph.add_plot(self.gyro_line_y)
         self.ids.gyro_graph.add_plot(self.gyro_line_z)
@@ -94,55 +55,59 @@ class SensorsBoxLayout(BoxLayout):
         self.ids.control_graph.add_plot(self.control_plot_integral_error)
         self.ids.control_graph.add_plot(self.control_plot_u)
 
+    def roll_plots(self):
+        self.old_accel_line_x = self.accel_line_x
+        self.old_accel_line_y = self.accel_line_y
+        self.old_accel_line_z = self.accel_line_z
+        self.old_gyro_line_x  = self.gyro_line_x
+        self.old_gyro_line_y  = self.gyro_line_y
+        self.old_gyro_line_z  = self.gyro_line_z
+        self.old_control_plot_error          = self.control_plot_error         
+        self.old_control_plot_integral_error = self.control_plot_integral_error
+        self.old_control_plot_u              = self.control_plot_u             
+
+
     def refresh_plot(self, sensor_data):
         
-        if self.t > self.ids.accel_graph.xmax:
-            #reset t
-            self.t = 0
-            #clear the entire pt list 
-            del self.pt[:]
-            #clear the entire accel_px, accel_py and accel_pz lists
-            del self.accel_px[:]
-            del self.accel_py[:]
-            del self.accel_pz[:]
-            #clear the entire gyro_px, gyro_py and gyro_pz lists
-            del self.gyro_px[:]
-            del self.gyro_py[:]
-            del self.gyro_pz[:]
-            del self.control_plot_error.points[:]
-            del self.control_plot_integral_error.points[:]
-            del self.control_plot_u.points[:]
-            
-        #add new values to pt list
-        self.pt.append(self.t)
-        
         # ---------------- for ACCELEROMETER sensor --------------------
-        #add new values
-        self.accel_px.append(sensor_data['sensors']['x'])
-        self.accel_py.append(sensor_data['sensors']['y'])
-        self.accel_pz.append(sensor_data['sensors']['z'])
-        #refresh the plot for the accelerometer's x values
-        self.accel_line_x.points = zip(self.pt, self.accel_px)
-        #refresh the plot for the accelerometer's y values
-        self.accel_line_y.points = zip(self.pt, self.accel_py)
-        #refresh the plot for the accelerometer's z values
-        self.accel_line_z.points = zip(self.pt, self.accel_pz)
-        
+        self.accel_line_x.points.append([self.t, sensor_data['sensors']['x']])
+        self.accel_line_y.points.append([self.t, sensor_data['sensors']['y']])
+        self.accel_line_z.points.append([self.t, sensor_data['sensors']['z']])
         # ---------------- for GYROSCOPE sensor ----------------------
-        #add new values
-        self.gyro_px.append(sensor_data['sensors']['gx'])
-        self.gyro_py.append(sensor_data['sensors']['gy'])
-        self.gyro_pz.append(sensor_data['sensors']['gz'])
-        #refresh the plot for the gyroerometer's x values
-        self.gyro_line_x.points = zip(self.pt, self.gyro_px)
-        #refresh the plot for the gyroerometer's y values
-        self.gyro_line_y.points = zip(self.pt, self.gyro_py)
-        #refresh the plot for the gyroerometer's z values
-        self.gyro_line_z.points = zip(self.pt, self.gyro_pz)
+        self.gyro_line_x.points.append ([self.t, sensor_data['sensors']['gx']])
+        self.gyro_line_y.points.append ([self.t, sensor_data['sensors']['gy']])
+        self.gyro_line_z.points.append ([self.t, sensor_data['sensors']['gz']])
     
         self.control_plot_error.points.append          ([self.t, sensor_data['control']['error']])
         self.control_plot_integral_error.points.append ([self.t, sensor_data['control']['integral_error']])
         self.control_plot_u.points.append              ([self.t, sensor_data['control']['u']])
+
+        if self.t > self.ids.accel_graph.xmax:
+            self.roll_plots()
+            self.create_plots()
+            #reset t
+            self.t = 0
+
+        if len(self.old_accel_line_x.points) > 0:
+            del self.old_accel_line_x.points[0]
+            del self.old_accel_line_y.points[0]
+            del self.old_accel_line_z.points[0]
+            del self.old_gyro_line_x.points[0]
+            del self.old_gyro_line_y.points[0]
+            del self.old_gyro_line_z.points[0]
+            del self.old_control_plot_error.points[0]
+            del self.old_control_plot_integral_error.points[0]
+            del self.old_control_plot_u.points[0]
+        else:
+            self.ids.accel_graph.add_plot(self.old_accel_line_x)
+            self.ids.accel_graph.add_plot(self.old_accel_line_y)
+            self.ids.accel_graph.add_plot(self.old_accel_line_z)
+            self.ids.gyro_graph.add_plot(self.old_gyro_line_x)
+            self.ids.gyro_graph.add_plot(self.old_gyro_line_y)
+            self.ids.gyro_graph.add_plot(self.old_gyro_line_z)
+            self.ids.control_graph.add_plot(self.old_control_plot_error)
+            self.ids.control_graph.add_plot(self.old_control_plot_integral_error)
+            self.ids.control_graph.add_plot(self.old_control_plot_u)
 
 class PlotSensorsApp(App):
     k = 0 #used to generate dummy values for sensors.
@@ -177,6 +142,12 @@ class PlotSensorsApp(App):
         return axes
 
     def get_fake_sensor_data(self):
+        log_values = []
+        for i in range(1, 5):
+            log_values.append(self.get_fake_sensor_data_single_point())
+        return log_values[1]
+
+    def get_fake_sensor_data_single_point(self):
         # generate some dummy values for sensors' data
         dummy_accel_x_values = exp(-(self.k - 0.5) ** 2 / (2 * .25 ** 2))
         dummy_accel_y_values = exp(-(self.k - 0.5) ** 4 / (2 * .25 ** 2))
